e0e323412c55d7c701c97d41fb25bbfb11347811
Pdhuney <136072169+Pdhuney@users.noreply.github.com>
2024-02-03 22:24:28 +0100

    Add files via upload

---
+++ homepage/script.js
@@ -0,0 +1,14 @@
+var nameNode = document.getElementById("name")
+nameNode.addEventListener("click", function(){
+    alert("Klik op de header voor meer informatie")
+});
+
+var nameNode = document.getElementById("name1")
+nameNode.addEventListener("click", function(){
+    alert("Klik op de header voor meer informatie")
+});
+
+var nameNode = document.getElementById("name2")
+nameNode.addEventListener("click", function(){
+    alert("Klik op de header voor meer informatie")
+});
